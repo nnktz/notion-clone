f1d4950642d979df6aedae8bd946a8d9f7787d7f
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-27 15:15:56 +0700

    Authentication & Database

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/provider/theme-provider';
+import { ConvexClientProvider } from '@/components/provider/convex-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -35,14 +36,16 @@ export default function RootLayout({
       lang='en'
       suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-          storageKey='jotion-theme-2'>
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+            storageKey='jotion-theme-2'>
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
