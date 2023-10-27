3d2b306ce02ed2249c5b8605b3256e576f04c65d
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-27 15:15:56 +0700

    Authentication

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import Link from 'next/link';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+
 import { cn } from '@/lib/utils';
 import { useScrollTop } from '@/hooks/use-scroll-top';
+import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode-toggle';
+import { Spinner } from '@/components/spinner';
 import { Logo } from './logo';
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +24,34 @@ export const Navbar = () => {
       <Logo />
 
       <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button
+                variant='ghost'
+                size='sm'>
+                Log in
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode='modal'>
+              <Button size='sm'>Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button
+              variant='ghost'
+              size='sm'
+              asChild>
+              <Link href='/documents'>Enter Jotion</Link>
+            </Button>
+
+            <UserButton afterSignOutUrl='/' />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
