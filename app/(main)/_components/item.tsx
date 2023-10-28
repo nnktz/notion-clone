bfdfb78b4464004ae1dec08daaeea94ad6478340
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-28 16:55:19 +0700

    Document list

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react';
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 import { Id } from '@/convex/_generated/dataModel';
+import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface ItemProps {
   id?: Id<'documents'>;
@@ -18,7 +23,7 @@ interface ItemProps {
   icon: LucideIcon;
 }
 
-export const Item: React.FC<ItemProps> = ({
+export const Item = ({
   label,
   onClick,
   icon: Icon,
@@ -29,7 +34,41 @@ export const Item: React.FC<ItemProps> = ({
   isSearch,
   level = 0,
   onExpand,
-}) => {
+}: ItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+
+    if (!id) {
+      return;
+    }
+
+    const promise = create({ title: 'Untitled', parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+
+        // router.push(`/documents/${documentId}`);
+      }
+    );
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note.',
+    });
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
@@ -43,8 +82,8 @@ export const Item: React.FC<ItemProps> = ({
       )}>
       {!!id && (
         <div
-          onClick={() => {}}
-          className='h-full rounded-sm hover:bg-neutral-300 dark:bg bg-neutral-600 mr-1'
+          onClick={handleExpand}
+          className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
           role='button'>
           <ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
         </div>
@@ -63,6 +102,28 @@ export const Item: React.FC<ItemProps> = ({
           <span className='text-xs'>⌘</span>K
         </kbd>
       )}
+
+      {!!id && (
+        <div className='ml-auto flex items-center gap-x-2'>
+          <div
+            role='button'
+            onClick={onCreate}
+            className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'>
+            <Plus className='h-4 w-4 text-muted-foreground' />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{ paddingLeft: level ? `${level * 12 + 25}px` : '12px' }}
+      className='flex gap-x-2 py-[3px]'>
+      <Skeleton className='h-4 w-4' />
+      <Skeleton className='h-4 w-[30%]' />
     </div>
   );
 };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -8,18 +8,18 @@ import {
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
-import { useMutation, useQuery } from 'convex/react';
+import { useMutation } from 'convex/react';
 import { toast } from 'sonner';
 
 import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
 import { UserItem } from './user-item';
 import { Item } from './item';
+import { DocumentList } from './document-list';
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -170,9 +170,7 @@ export const Navigation = () => {
         </div>
 
         <div className='mt-4'>
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
