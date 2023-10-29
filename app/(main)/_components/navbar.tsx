76cd764673d169b8d7d66e8a21ad6be3c738e1f7
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-29 18:14:28 +0700

    Preview page (Publish)

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { Id } from '@/convex/_generated/dataModel';
 import { Title } from './title';
 import { Banner } from './banner';
 import { Menu } from './menu';
+import { Publish } from './publish';
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -26,7 +27,7 @@ export const Navbar: React.FC<NavbarProps> = ({
 
   if (document === undefined) {
     return (
-      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py2 w-full flex items-center justify-between'>
+      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between'>
         <Title.Skeleton />
 
         <div className='flex items-center gap-x-2'>
@@ -41,7 +42,7 @@ export const Navbar: React.FC<NavbarProps> = ({
   }
   return (
     <>
-      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py2 w-full flex items-center gap-x-4'>
+      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4'>
         {isCollapsed && (
           <MenuIcon
             role='button'
@@ -54,6 +55,7 @@ export const Navbar: React.FC<NavbarProps> = ({
           <Title initialData={document} />
 
           <div className='flex items-center gap-x-2'>
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -9,7 +9,7 @@ import {
   Settings,
   Trash,
 } from 'lucide-react';
-import { useParams, usePathname } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 import { useMutation } from 'convex/react';
@@ -31,6 +31,7 @@ import { TrashBox } from './trash-box';
 import { Navbar } from './navbar';
 
 export const Navigation = () => {
+  const router = useRouter();
   const settings = useSettings();
   const search = useSearch();
   const params = useParams();
@@ -134,7 +135,9 @@ export const Navigation = () => {
   };
 
   const handleCreate = () => {
-    const promise = create({ title: 'Untitled' });
+    const promise = create({ title: 'Untitled' }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
     toast.promise(promise, {
       loading: 'Creating a new note...',
